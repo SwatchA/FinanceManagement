4b9e097247504f73242c7e588e0a6d9bfa28268f
SwatchA <swatcha4561@gmail.com>
2024-11-01 03:19:42 +0600

    vue firebase project initialized

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default{
+  apiKey: "AIzaSyByc5ln7kHeMMdD-ADxO9OnBjODHuh_Xn4",
+  authDomain: "financeman-c3a97.firebaseapp.com",
+  projectId: "financeman-c3a97",
+  storageBucket: "financeman-c3a97.firebasestorage.app",
+  messagingSenderId: "374990049943",
+  appId: "1:374990049943:web:e86b84231680b6610bcfd2"
+};
